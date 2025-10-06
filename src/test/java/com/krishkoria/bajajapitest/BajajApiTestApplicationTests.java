b8c78813e1b7a19718d02b134bf32223c8ae1c96
Krish Koria <krishkoria2004@gmail.com>
2025-10-06 16:13:52 +0530

    initialize BajajApiTest project with Gradle, Spring Boot, and basic configurations

---
+++ src/test/java/com/krishkoria/bajajapitest/BajajApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishkoria.bajajapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
